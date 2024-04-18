f2ebda0476b005a25052a665b48fd658235b9082
MoyBadillo117 <A00834306@tec.mx>
2024-04-18 00:56:22 -0600

    Primer avance metric card
    Se implementó la metric card

---
+++ connectmate/src/components/MetricCard/MetricCard.tsx
@@ -0,0 +1,20 @@
+import { IMetricCard } from "./types";
+
+import './styles.css'
+
+const MetricCard: React.FC<IMetricCard> = ({ title, subtitle, metricUnit }) => {
+    return (
+        <div className="card">
+            <div className="topPart">
+                <h1>{title}</h1>
+                <h1 className="unit">{metricUnit}</h1>
+            </div>
+            <div className="line"></div>
+            <div className="downPart">
+                {subtitle}
+            </div>
+        </div>
+    );
+};
+
+export default MetricCard;
---
+++ connectmate/src/components/MetricCard/types.ts
@@ -0,0 +1,5 @@
+export interface IMetricCard {
+    title: string,
+    subtitle: string,
+    metricUnit: string; // 'ns', 'n%', 'n'
+}

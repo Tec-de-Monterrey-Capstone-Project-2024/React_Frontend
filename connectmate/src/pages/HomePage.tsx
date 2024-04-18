3e8490a410f99075e75b4d3dd7f801ffa9f679de
MoyBadillo117 <A00834306@tec.mx>
2024-04-18 16:20:34 -0600

    Update HomePage.tsx

--- connectmate/src/pages/HomePage.tsx
+++ connectmate/src/pages/HomePage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../ROUTES';
-import { MetricCard } from '../components/MetricCard';
+//import { MetricCard } from '../components/MetricCard';
 
 const HomePage: React.FC = () => {
   return <>
     <div>
-      <MetricCard title="TU PUTA MADRE" subtitle="MURIO" metricUnit="n"/>
-      <MetricCard title="TU PUTA MADRE" subtitle="MURIO" metricUnit="n"/>
       <h1>Welcome to the Home Page</h1>
       <Link to={ROUTES.AUTH}>auth</Link>
       <br />

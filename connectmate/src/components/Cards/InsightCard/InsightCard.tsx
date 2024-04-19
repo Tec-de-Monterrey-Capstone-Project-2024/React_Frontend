9a7cee735025c541a1d5f8691c8109c2735da849
LuisGerMagana <a01662420@tec.mx>
2024-04-19 17:01:58 -0600

    InsightCard
    componente de insight description

---
+++ connectmate/src/components/Cards/InsightCard/InsightCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+interface InsightProps {
+    title: string;
+    message: string;
+}
+
+const Insight: React.FC<InsightProps> = ({ title, message }: InsightProps) => {
+    return (
+        <div className="insight-container">
+            <h2>{title}</h2>
+            <p>{message}</p>
+        </div>
+    );
+};
+
+export default Insight;
---
+++ connectmate/src/components/Cards/InsightCard/index.ts
@@ -0,0 +1,1 @@
+import InsightCard from './InsightCard';

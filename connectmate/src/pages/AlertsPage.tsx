369bacfa8c3244f22dcbb79375279cb2c29162ea
Aram32mm <a01657142@tec.mx>
2024-04-22 21:50:53 -0600

    changes

--- connectmate/src/pages/AlertsPage.tsx
+++ connectmate/src/pages/AlertsPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '../ROUTES';
-import AlertsTable from '../components/Tables/AlertsTable';
+import AlertsTable from '../components/Tables/AlertTable';
 //import { MetricCard } from '../components/MetricCard';
 
 const HomePage: React.FC = () => {

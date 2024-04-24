acb6dd8712ab020d99c4940230ac0952bf23ec56
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-04-23 20:02:46 -0600

    Update

--- connectmate/src/components/Button/types.tsx
+++
@@ -1,8 +0,0 @@
-
-export interface ButtonProps {
-  children: React.ReactNode;
-  onClick: () => void;
-  className?: string;
-  type?: 'button' | 'submit' | 'reset';
-  variant: 'light' | 'dark';
-}

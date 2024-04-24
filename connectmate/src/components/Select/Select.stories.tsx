497d942d8dea0cfdf3a0d78b7d6da057c2b81ff5
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-04-24 16:46:35 -0600

    Revert "Merge branch 'main' into dev"
    This reverts commit 4ea5c8e2e3e0027176386292e1034abaa6d85853, reversing
    changes made to f97b54e7e166367f989b5e650923611972030521.

---
+++ connectmate/src/components/Select/Select.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, StoryFn } from "@storybook/react";
+import Select from "./Select";
+import { ISelect } from "./types";
+import React from 'react';
+
+const meta = {
+    title: 'Components/Select',
+    component: Select,
+    parameters: {
+        layout: 'fullscreen',
+        docs: {
+            story: {
+                inline: false,
+                iframeHeight: 400,
+
+            }
+        },
+    },
+    argTypes: {
+        placeholder: { control: 'text' },
+        color: {
+            options: ['green', 'gray'],
+            control: { type: 'select' },
+        },
+    },
+    tags: ["autodocs"]
+} as Meta;
+
+export default meta;
+
+const Template: StoryFn<ISelect> = (args) => <Select {...args} />;
+
+export const Filters = Template.bind({});
+Filters.args = {
+    placeholder: 'Filters',
+    color: 'green',
+};
+
+export const SelectorValues = Template.bind({});
+SelectorValues.args = {
+    color: 'gray',
+}

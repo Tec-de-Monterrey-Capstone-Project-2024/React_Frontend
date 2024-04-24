77ae658c030af004912ca507b27fe96cf24b42de
Caro González <a01662120@tec.mx>
2024-04-24 13:13:08 -0600

    Button v3.1

--- connectmate/src/components/Button/Button.tsx
+++ connectmate/src/components/Button/Button.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
+import './styles.css';
 import { ButtonProps } from './types';
 
-<<<<<<< HEAD
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
@@ -16,22 +15,12 @@ const Button: React.FC<ButtonProps> = ({
   <button
     className={`button ${variant} ${color} ${className}`.trim()} // ${color} 
     onClick={onClick}
-    type={type as "button" | "submit" | "reset" | undefined} // Fix: Update type prop
+    type={type}
     title={title} // Cambio
     content={content} // Cambio
   >
-=======
-import './styles.css';
-
-const Button: React.FC<ButtonProps> = ({ children, onClick, className = '', type = 'button', variant }) => (
-  <button className={`button ${variant} ${className}`.trim()} onClick={onClick} type={type}>
->>>>>>> 518b638b15fd0505539efbacb0eef87f06e02df1
     {children}
   </button>
 );
 
-<<<<<<< HEAD
 export default Button;
-=======
-export default Button;
->>>>>>> 518b638b15fd0505539efbacb0eef87f06e02df1
--- connectmate/src/components/Button/types.tsx
+++ connectmate/src/components/Button/types.tsx
@@ -2,14 +2,20 @@ export interface ButtonProps {
   children: React.ReactNode;
   onClick: () => void;
   className?: string;
+  type?: 'button' | 'submit' | 'reset';
+  color?: string; // Cambio
+  title?: string; // Cambio
+  content?: string; // Cambio
+  variant: 'grey' | 'darkblue' | 'pink' | 'green' | 'yellow' | 'lightgreen';
+}
+/*
+export interface ButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
   type?: 'button' | 'done' | 'ignore' | 'solve' | 'alert';
   color?: string; // Cambio
   title?: string; // Cambio
   content?: string; // Cambio
-<<<<<<< HEAD
-  variant: 'grey' | 'dark blue' | 'pink' | 'lightgreen' | 'yellow' | 'darkgreen';
-=======
-  // variant: 'grey' | 'dark blue' | 'pink' | 'green';
   variant: 'light' | 'dark';
->>>>>>> 518b638b15fd0505539efbacb0eef87f06e02df1
-}
+} */

887f1e51a0a10a111d18bbf75baf889d92e37b92
Julia Pasten Da Silva A01660665 <113377269+JuliaPasten@users.noreply.github.com>
2024-04-25 11:09:58 -0600

    Toto listo para el merge

--- connectmate/src/components/Button/types.tsx
+++ connectmate/src/components/Button/types.tsx
@@ -6,16 +6,5 @@ export interface ButtonProps {
   color?: string; // Cambio
   title?: string; // Cambio
   content?: string; // Cambio
-  variant: 'grey' | 'darkblue' | 'pink' | 'green' | 'yellow' | 'lightgreen' | 'light' | 'dark';
+  variant: 'grey' | 'darkblue' | 'pink' | 'green' | 'yellow' | 'lightgreen' | 'light' | 'dark'|'darkgreen';
 }
-/*
-export interface ButtonProps {
-  children: React.ReactNode;
-  onClick: () => void;
-  className?: string;
-  type?: 'button' | 'done' | 'ignore' | 'solve' | 'alert';
-  color?: string; // Cambio
-  title?: string; // Cambio
-  content?: string; // Cambio
-  variant: 'light' | 'dark';
-} */

ce5520da7db8857dd5d16a76b0a6f3f4cc68369e
LuisGerMagana <a01662420@tec.mx>
2024-04-24 20:12:19 -0600

    cambios

--- connectmate/src/components/Cards/InsightCardDescription/InsightDescription.tsx
+++ connectmate/src/components/Cards/InsightCardDescription/InsightDescription.tsx
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import './style.css';
 
@@ -16,3 +17,35 @@ const Insight: React.FunctionComponent<InsightProps> = ({ title, message }: Insi
 };
 
 export default Insight;
+*/
+
+import './style.css';
+import React from 'react';
+
+interface InsightDescriptionProps {
+    title: string;
+    message1: string;
+    message2: string;
+    containerStyle?: React.CSSProperties;
+    titleStyle?: React.CSSProperties;
+}
+
+const InsightDescription: React.FC<InsightDescriptionProps> = ({
+    title,
+    message1,
+    message2,
+    containerStyle,
+    titleStyle,
+}) => {
+    return (
+        <div className="insight-container" style={containerStyle}>
+            <h2 style={titleStyle}>{title}</h2>
+            <div>
+                <p>{message1}</p>
+                <p>{message2}</p>
+            </div>
+        </div>
+    );
+};
+
+export default InsightDescription;

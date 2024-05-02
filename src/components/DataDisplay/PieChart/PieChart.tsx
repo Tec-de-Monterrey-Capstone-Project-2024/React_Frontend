a58a417668a4bbf12ec6ac4b751b62ca847a3185
abeejita <a01655892@itesm.com>
2024-05-02 14:35:11 -0600

    Pie chart ready.

--- src/components/DataDisplay/PieChart/PieChart.tsx
+++ src/components/DataDisplay/PieChart/PieChart.tsx
@@ -1,25 +1,24 @@
-import React, { useState, useEffect} from "react";
-
+import React from "react";
 import { IPieChart } from "./types";
 import { PieChart } from '@mui/x-charts/PieChart';
 
 const Pie: React.FC<IPieChart> = ({ id, value, metric }) => {
+    const remainingValue = 100 - value;
 
     const series = [{
-        data: [{
-            id: id,
-            value: value,
-            label: metric
-        }]
+        data: [
+            { id, value, label: `${metric} (${value}%)` },
+            { id: `${id}-remainder`, value: remainingValue }
+        ]
     }];
 
     return (
         <PieChart
-            series= {series}
+            series={series}
             width={400}
             height={200}
         />
-    )
+    );
 }
 
 export default Pie;

ce0c96beed818aac6a46f7219cae4d9cfdbb26b0
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-05-02 17:06:09 -0600

    Update

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,10 +5,9 @@ import { ROUTES } from '../ROUTES';
 const HomePage: React.FC = () => {
   return <>
     <div>
-      <h1>Welcome to the Home Page</h1>
+
       <Link to={ROUTES.AUTH}>Auth</Link>
-      <br />
-      <Link to={ROUTES.DASHBOARD}>Dashboard</Link>
+
     </div>
   </>;
 };

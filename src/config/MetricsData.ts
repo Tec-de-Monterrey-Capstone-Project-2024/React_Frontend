b05d7cbd27c54761a6f79fc68bf132d5be324fd1
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-05-02 16:31:01 -0600

    Update

--- src/config/MetricsData.ts
+++ src/config/MetricsData.ts
@@ -1,5 +1,4 @@
 export type MetricData = {
-    metric_info_code: string;
     name: string;
     min: number;
     max: number;
@@ -7,65 +6,57 @@ export type MetricData = {
     graph: 'Pie' | 'Gauge';
 };
 
-const MetricsData: Record<number, MetricData> = {
-    1: {
-        metric_info_code: "SL",
+const MetricsData: Record<string, MetricData> = {
+    "SL": {
         name: "Service Level",
         min: 70,
         max: 100,
         positive_upside: true,
         graph: 'Pie'
     },
-    2: {
-        metric_info_code: "ACR",
+    "ACR": {
         name: "Abandonment Call Rate",
         min: 0,
         max: 30,
         positive_upside: false,
         graph: 'Pie'
     },
-    3: {
-        metric_info_code: "FCR",
+    "FCR": {
         name: "First Contact Resolution",
         min: 70,
         max: 100,
         positive_upside: true,
         graph: 'Pie'
     },
-    4: {
-        metric_info_code: "OCC",
+    "OCC": {
         name: "Occupancy",
         min: 0,
         max: 30,
         positive_upside: false,
         graph: 'Pie'
     },
-    5: {
-        metric_info_code: "SA",
+    "SA": {
         name: "Schedule Adherence",
         min: 70,
         max: 90,
         positive_upside: true,
         graph: 'Pie'
     },
-    6: {
-        metric_info_code: "ASA",
+    "ASA": {
         name: "Average Speed Answer",
         min: 0,
         max: 60,
         positive_upside: false,
         graph: 'Gauge'
     },
-    7: {
-        metric_info_code: "AHT",
+    "AHT": {
         name: "Average Handle Time",
         min: 5,
         max: 20,
         positive_upside: false,
         graph: 'Gauge'
     },
-    8: {
-        metric_info_code: "VFR",
+    "VFR": {
         name: "Virtual Floor Reconfiguration",
         min: 0.5,
         max: 2,

60f45903e2b54c8bb8f407157692f0ffa7464dba
Julia Pasten Da Silva A01660665 <113377269+JuliaPasten@users.noreply.github.com>
2024-05-16 13:18:45 -0600

    Ya hice la vista de la HU que faltaba modificar

--- src/components/Cards/InsightCardDescription/InsightDescription.tsx
+++ src/components/Cards/InsightCardDescription/InsightDescription.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
-interface InsightProps {
-    title: string;
-    message: string;
+interface InsightDescriptionProps {
+  title: string;
+  message: string;
+  situationTitle: string;
+  actionTitle: string;
 }
 
-const Insight: React.FunctionComponent<InsightProps> = ({ title, message }: InsightProps) => {
-    return (
-        <div className="insight-container rounded-lg">
-            <h2>{title}</h2>
-            <hr className="border-t-4 border-grey mb-4" />
-            <span>{message}</span>
+const InsightDescription: React.FC<InsightDescriptionProps> = ({ title, message, situationTitle, actionTitle }) => {
+  return (
+    <div className="p-6 border border-gray-300 rounded-lg shadow-lg bg-gray-50">
+      <h2 className="text-4xl font-bold mb-4 flex items-center">
+        <span className="p-2 bg-gray-50 rounded-full text-lime-500 text-4xl  mr-2">
+          <FontAwesomeIcon icon={faBell} />
+        </span>
+        {title}
+      </h2>
+      <hr className="border-t-2 border-gray-250 my-4" />
+      <div className="mt-4">
+        <div className="mb-2">
+          <strong className="text-2xl text-black">{situationTitle}</strong>
         </div>
-    );
+        <p className="text-lg text-gray-800">{message}</p>
+        <div className="mt-4">
+        <hr className="border-t-2 border-gray-150 my-2" />
+          <strong className="text-2xl text-black">{actionTitle}</strong>
+        </div>
+        <p className="text-lg text-gray-800">Consider allocating more agents from the Receipts Queue to the Reimbursements Queue.</p>
+      </div>
+    </div>
+  );
 };
 
-export default Insight;
+export default InsightDescription;
--- src/pages/AlertViewMore.tsx
+++ src/pages/AlertViewMore.tsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../components/Button/Button';
 import Insight from '../components/Cards/InsightCardDescription/InsightDescription';
 
 const AlertViewMore: React.FC = () => {
-    const navigate = useNavigate();
-    const goBack = () => {
-        navigate(-1);
-      };
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
 
-      return (
-        <div className="p-4 flex flex-col h-screen">
-          <div>
-            <button onClick={goBack} className="flex items-center text-black font-sans text-base no-underline">
-              <span className="mr-2">&#8592;</span>
-              Back
-            </button>
-            <br />
-          </div>
-          <div className="flex-grow">
-            <Insight title={'Reconfigure Virtual floor'} message={'Not Enough people on the Reimbursements Queue.'} />
-          </div>
-          <div className="flex justify-center space-x-4 mt-auto mb-24">
-            <Button variant="darkblue" onClick={() => {}}>Solve in Connect</Button>
-            <Button variant="green" onClick={() => {}}>In progress</Button>
-            <Button variant="grey" onClick={() => {}}>Done</Button>
-          </div>
-        </div>
-      );
-}
+  return (
+    <div className="p-4 flex flex-col h-screen">
+      <div>
+        <button onClick={goBack} className="flex items-center text-black font-sans text-base no-underline">
+          <span className="mr-2">&#8592;</span>
+          Back
+        </button>
+        <br />
+      </div>
+      <div className="flex-grow">
+        <Insight 
+          title="Reconfigure Virtual floor" 
+          message="Not Enough people on the Reimbursements Queue." 
+          situationTitle="Situación" 
+          actionTitle="Acción"
+        />
+      </div>
+      <div className="flex justify-center space-x-4 mt-auto mb-24">
+        <Button variant="darkblue" onClick={() => {}}>Solve in Connect</Button>
+        <Button variant="green" onClick={() => {}}>In progress</Button>
+        <Button variant="grey" onClick={() => {}}>Done</Button>
+      </div>
+    </div>
+  );
+};
 
-export default AlertViewMore
+export default AlertViewMore;

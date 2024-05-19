16c25ecb119f907a3d89ab7f837d50a23c506a7d
fernando2504 <a01642285@tec.mx>
2024-05-18 18:46:51 -0600

    context and authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 
 import Layout from './components/Layouts/Layout';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './context/PrivateRoute';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import AgentDashboardPage from './pages/AgentDashboardPage/AgentDashboardPage';
 import AgentsPage from './pages/AgentsPage/AgentsPage';
-import ForgotPage from './pages/ForgotPage';
+
 import AccountPage from './pages/AccountPage/AccountPage';
 import AlertsPage from './pages/AlertsPage';
 import QueuesPage from './pages/QueuesPage/QueuesPage';
@@ -16,20 +19,25 @@ import QueuesPage from './pages/QueuesPage/QueuesPage';
 const App: React.FC = () => {
   return (
     <Router>
+      <AuthProvider>
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Navigate to='/auth' />} />
           <Route path="/auth" element={<LoginPage />} />
-          <Route path="/auth/forgot" element={<ForgotPage />} />
-          <Route path="/queues" element={<QueuesPage/>} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/dashboard/agent/:id" element={<AgentDashboardPage />} />
-          <Route path="/agents" element={<AgentsPage />} />
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/alerts" element={<AlertsPage />} />
+
+          <Route path='/' element={<PrivateRoute />}>
+            <Route path="/home" element={<HomePage/>} />
+            <Route path="/queues" element={<QueuesPage/>} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/dashboard/agent/:id" element={<AgentDashboardPage />} />
+            <Route path="/agents" element={<AgentsPage />} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/alerts" element={<AlertsPage />} />
+          </Route>
           {/* <Route component={NotFound} /> */}
         </Routes>
       </Layout>
+      </AuthProvider>
     </Router>
   );
 };
--- src/components/Forms/LoginForgot/LoginForgot.tsx
+++ src/components/Forms/LoginForgot/LoginForgot.tsx
@@ -1,19 +1,35 @@
 import React, { useState } from "react";
 import { formProps } from "./types";
+import {signInWithEmailAndPassword} from 'firebase/auth';
+import { auth } from "../../../firebase";
 
 import './styles.css';
+import { useNavigate } from "react-router-dom";
 
 const LoginForgot: React.FC<formProps> = ({ title, label, label2, button, link }) => {
-    const [formData, setFormData] = useState({
-        name: '',
-        password: ''
-    });
-
+    const navigate = useNavigate();
     const [showLoginForgot, setShowLoginForgot] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleChangeForm = () => {
-        setShowLoginForgot(!showLoginForgot);
-    };
+    const handleChangeForm = () => {setShowLoginForgot(!showLoginForgot);};
+
+    const onLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            navigate("/home")
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+        });
+       
+    }
 
     const loginProps: formProps = {
         title: "Login",
@@ -33,24 +49,24 @@ const LoginForgot: React.FC<formProps> = ({ title, label, label2, button, link }
     
     return (
         <div className="card" >
-            <form className=" ">
+            <form className=" " onSubmit={onLogin}>
                     <div className="loginforgot-text">
                         <h3>{showLoginForgot ? forgotPasswordProps.title : loginProps.title}</h3>
                     </div>
                     <div className="">
                     <div className="input-container ">
                         <label className="input-label" htmlFor="iam-role">{showLoginForgot ? forgotPasswordProps.label : loginProps.label}</label>
-                        <input className="input" type="text" id="iam-role" placeholder="  iam role..." />
+                        <input className="input" name="email" type="email" id="email-address" placeholder="  email address..." onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     {Object.keys(loginProps).includes("label2") && !showLoginForgot && (
                         <div className="input-container">
                             <label className="input-label" htmlFor="password">{showLoginForgot ? forgotPasswordProps.label : loginProps.label2}</label>
-                            <input className="input" type="password" id="password" placeholder="  password..."  />
+                            <input className="input" name="password" required type="password" id="password" placeholder="  password..." onChange={(e) => setPassword(e.target.value)}  />
                         </div>
                     )}
                 </div>
                 <div className="link-container">
-                    <button className="button login-button">{showLoginForgot ? forgotPasswordProps.button : loginProps.button}</button>
+                    <button className="button login-button" type="submit">{showLoginForgot ? forgotPasswordProps.button : loginProps.button}</button>
                     <a className="link"  onClick={handleChangeForm}>{showLoginForgot ? forgotPasswordProps.link : loginProps.link}</a>
                 </div>
             </form>

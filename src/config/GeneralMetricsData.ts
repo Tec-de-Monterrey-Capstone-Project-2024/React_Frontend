8d14b0ce53e78a0627210ccf93b077cbc7b6e188
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-05-20 19:52:09 -0600

    Re-estructuration of the dummy JSON for obtain general metric data

--- src/config/GeneralMetricsData.ts
+++ src/config/GeneralMetricsData.ts
@@ -4,58 +4,42 @@ const dummyGeneralMetrics: IMetric[] = [
     {
         "id": 1,
         "metric_info_code": "SL",
-        "value": 100,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 100
     },
     {
         "id": 2,
         "metric_info_code": "ACR",
-        "value": 10,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 10
     },
     {
         "id": 3,
         "metric_info_code": "FCR",
-        "value": 99,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 99
     },
     {
         "id": 4,
         "metric_info_code": "OCC",
-        "value": 50,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 50
     },
     {
         "id": 5,
         "metric_info_code": "SA",
-        "value": 95,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 95
     },
     {
         "id": 6,
         "metric_info_code": "ASA",
-        "value": 70,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 70
     },
     {
         "id": 7,
         "metric_info_code": "AHT",
-        "value": 30,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 30
     },
     {
         "id": 8,
         "metric_info_code": "VFR",
-        "value": 23,
-        "agent_id": null,
-        "queue_id": 1
+        "value": 23
     }
 ]
 
--- src/services/metrics/types.ts
+++ src/services/metrics/types.ts
@@ -2,6 +2,6 @@ export interface IMetric {
     id: number,
     metric_info_code: string,
     value: number,
-    agent_id: number | null,
-    queue_id: number | null
+    agent_id?: number | null,
+    queue_id?: number | null
 }

0938203e59f62c297b9deb6aa21f05a4920b9814
AndreaAguilar <a01656200@tec.mx>
2024-05-21 07:56:38 -0600

    Update InsightCard.stories.tsx

--- src/components/Cards/InsightCard/InsightCard.stories.tsx
+++ src/components/Cards/InsightCard/InsightCard.stories.tsx
@@ -11,7 +11,7 @@ const meta = {
         docs: {
             story: {
                 inline: true,
-                iframeHeight: 400,
+                iframeHeight: 200,
             }
         },
     },

f7652f950ce1f521f7f61fb515054302a0c7bdaa
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-05-24 12:55:21 -0600

    First advance with navbar

--- src/components/Layouts/Navbar.tsx
+++ src/components/Layouts/Navbar.tsx
@@ -1,13 +1,42 @@
 import { Button } from '../Button';
 import { Select }  from '../Widgets/Select';
+import { useDataContext } from '../../context/DataContext';
 import agentIcon from '../../assets/icons/agent.svg';
 import alertIcon from '../../assets/icons/alert.svg';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [instances, setInstances] = useState<IInstance[]>([]);
+  const { selectedInstanceId, setSelectedInstanceId } = useDataContext();
+  useEffect(() => {
+    const fetchInstances = async () => {
+      var res = await getInstances();
+      setInstances(res.data);
+    }
+    fetchInstances();
+  }, []);
+  const changeInstance = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedInstanceId(event.target.value);
+    setSelectedQueueId('all');
+  };
+
+  const [queues, setQueues] = useState<IQueue[]>([]);
+  const { selectedQueueId, setSelectedQueueId } = useDataContext();
+  useEffect(() => {
+    const fetchQueues = async () => {
+      var res = await getQueues(selectedInstanceId);
+      setQueues(res.data);
+    }
+    if (selectedInstanceId !== "0") {
+      fetchQueues();
+    }
+  }, [selectedInstanceId]);
+  const changeQueue = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedQueueId(event.target.value);
+  };
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState<string | null>(null);
 
@@ -80,8 +109,28 @@ const Navbar: React.FC = () => {
           <div className='links'>
             <Button variant="light" onClick={() => {}} className="green icon">
               <img src={alertIcon} alt="Alert icon" />
-            </Button> 
-            <Select placeholder="Filters" color="green"></Select>
+            </Button>
+
+            {(selectedInstanceId !== "0") && (
+              <select id="queues" title='queues' value={selectedQueueId} onChange={changeQueue} className='btn-type-2 light'>
+                <option value="all">All queues</option>
+                {queues.map((queue) => (
+                  <option key={queue.id} value={queue.id}>
+                    {queue.name}
+                  </option>
+                ))}
+              </select>
+            )}
+
+            <select id="instances" title='instances' value={selectedInstanceId} onChange={changeInstance} className='btn-type-2'>
+              <option value="0">Select instance</option>
+              {instances.map((instance) => (
+                <option key={instance.id} value={instance.id}>
+                  {instance.instanceAlias}
+                </option>
+              ))}
+            </select>
+
             <Button variant="light" onClick={() => {navigate("/account");}} className="green icon">
               <img src={agentIcon} alt="Agent icon" />
             </Button> 

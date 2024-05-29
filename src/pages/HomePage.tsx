cde9f1c93a83341ed15a4f0c10a4de44ff3890da
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-05-28 22:05:33 -0600

    Fixed errors
    Fixed errors on overlay.
    Fixed errors in ROUTES

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ROUTES } from '../routes';
+import { ROUTES } from '../ROUTES';
 
 const HomePage: React.FC = () => {
   return <>

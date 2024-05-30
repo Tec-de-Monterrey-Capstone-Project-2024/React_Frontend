15f018deed8e9cb0555c9753d44d8341050734ef
fernando2504 <a01642285@tec.mx>
2024-05-30 12:06:46 -0600

    error message update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import Layout from './components/Layouts/Layout';
 import PrivateRoute from './context/PrivateRoute';
+import { ErrorProvider } from './context/ErrorContext';
 
 import LoginPage from './pages/AuthPages/LoginPage';
 import SignupPage from './pages/AuthPages/SignupPage';
@@ -21,6 +22,7 @@ import InsightPage  from './pages/InsightPage/InsightPage';
 
 const App: React.FC = () => {
   return (
+    <ErrorProvider>
     <Router>
       <Layout>
         <Routes>
@@ -46,6 +48,7 @@ const App: React.FC = () => {
         </Routes>
       </Layout>
     </Router>
+    </ErrorProvider>
   );
 };
 
--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -3,21 +3,19 @@ import { Link, useLocation } from "react-router-dom";
 
 import { useDataContext } from "../../context/DataContext";
 import { useAuth } from "../../context/AuthContext";
+import { useError } from "../../context/ErrorContext";
 
 import { loginUser } from "../../services/user/loginUser";
 
 import './styles.css';
 
-
-
-
 const LoginForm: React.FC = () => {
     const { setUser } = useDataContext();
     const { signIn, signOut } = useAuth();
+    const { loginError, setLoginError } = useError();
 
     const [email, setEmail] = useState('a01657142@tec.mx');
     const [password, setPassword] = useState('password');
-    const [error, setError] = useState<string | null>(null);
 
     const location = useLocation();
     const fromForgotForm = location.state?.fromForgotForm || false;
@@ -29,14 +27,14 @@ const LoginForm: React.FC = () => {
             const res = await loginUser(firebaseId);
             if (res.status >= 200 && res.status < 300) {
                 setUser(res.data);
-                setError(null); 
+                setLoginError(null); 
             } else {
-                setError("Invalid credentials");
+                setLoginError("Invalid credentials");
                 signOut();
             }
-        } catch (error) {
-            setError("Invalid credentials");
-            console.error("Login failed:", error);
+        } catch (loginError) {
+            setLoginError("Invalid credentials");
+            console.error("Login failed:", loginError);
             signOut();
         }
     }
@@ -46,7 +44,6 @@ const LoginForm: React.FC = () => {
             <div className="title">
                 <h3>Login</h3>
                 {fromForgotForm && <p className="text-sm text-[--dark-red]">You have received an email with instructions, come back after resetting your password.</p>}
-
             </div>
             <div className="">
                 <div className="input-container">
@@ -76,7 +73,7 @@ const LoginForm: React.FC = () => {
                 </div>
             </div>
             <div className="link-container">
-                {error && (
+                {loginError && (
                     <p className="text-red-500 mb-4 text-sm font-medium">Invalid Credentials, try again.</p>
                 )}
                 <button className="button login-button" type="submit">Login</button>
@@ -86,6 +83,5 @@ const LoginForm: React.FC = () => {
         </form>
     );
 }
-
 
 export default LoginForm;

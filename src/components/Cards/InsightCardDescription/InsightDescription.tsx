18058dd5eaf0ad183179e46a83a3a1bd55306ef2
Julia Pasten Da Silva A01660665 <113377269+JuliaPasten@users.noreply.github.com>
2024-05-30 00:12:35 -0600

    Detail in style

--- src/components/Cards/InsightCardDescription/InsightDescription.tsx
+++ src/components/Cards/InsightCardDescription/InsightDescription.tsx
@@ -17,7 +17,7 @@ const InsightDescription: React.FC<IInsightDescription> = ({
     <div className="p-6 border border-gray-300 rounded-lg shadow-lg bg-gray-50">
       <h2 className="text-4xl font-bold mb-4 flex items-center">
         <span className="p-2 bg-gray-50 rounded-full text-lime-500 text-4xl border border-gray-300 mr-2">
-          <img src={alertIcon} alt="Alert icon" width={32} height={32} />
+          <img src={alertIcon } alt="Alert icon" width={32} height={32}  />
         </span>
         {title}
       </h2>
@@ -32,6 +32,7 @@ const InsightDescription: React.FC<IInsightDescription> = ({
           <strong className="text-2xl text-black">{actionTitle}</strong>
         </div>
         <p className="text-lg text-gray-800">Consider allocating more agents from the Receipts Queue to the Reimbursements Queue.</p>
+        <hr className="border-t-2 border-gray-150 my-2" />
         <div className="mt-4">
           <strong className="text-2xl text-black">Root Cause</strong>
           <p className="text-lg text-gray-800">{insightRootCause}</p>

60fa781e61dee0f90e703ec5d34cda4ded934003
Esteban Zepeda <126184363+a01026512@users.noreply.github.com>
2024-06-05 17:33:40 -0600

    Update AlertsPage.test.tsx

--- src/pages/AlertsPage/__tests__/AlertsPage.test.tsx
+++ src/pages/AlertsPage/__tests__/AlertsPage.test.tsx
@@ -5,8 +5,8 @@ import AlertsPage from '../AlertsPage';
 import { ROUTES } from '../../../ROUTES';
 
 // Mock the components and hooks from react-router-dom
-jest.mock('../../components/Tables/AlertTable', () => () => <div data-testid="alerts-table" />);
-jest.mock('../../components/Button', () => ({
+jest.mock('../../../components/Tables/AlertTable', () => () => <div data-testid="alerts-table" />);
+jest.mock('../../../components/Button', () => ({
   Button: ({ onClick, children }: { onClick: () => void; children: React.ReactNode }) => (
     <button onClick={onClick} data-testid="add-alert-button">
       {children}

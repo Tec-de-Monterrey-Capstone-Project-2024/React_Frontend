f82b0bfb17a048ba3ddcf315440f589217f55464
fernando2504 <a01642285@tec.mx>
2024-06-10 15:02:15 -0600

    Update AuthLayout.tsx

--- src/components/Layouts/AuthLayout/AuthLayout.tsx
+++ src/components/Layouts/AuthLayout/AuthLayout.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { IAuthLayout } from './types';
+import img from '../../../assets/img/logo.png'
+import loginFoto from '../../../../public/loginFoto.png'
 
 const AuthLayout: React.FC<IAuthLayout> = ({ children }) => {
   return (
     <div className='grid grid-rows-2 h-screen w-screen relative'>
       <div className='grid-span-1'>
-        <img className='h-full w-full' src='/loginFoto.png' alt='login page image'/>
+        <img className='h-full w-full' src={loginFoto} alt='login page image'/>
       </div>
       {children}
       <div className='flex justify-center items-end pb-5'>
-        <img className='grid-span-2' src='/logo.png' alt='logo image' />
+        <img className='grid-span-2' src={img} alt='logo image' />
       </div>
     </div>
   )
